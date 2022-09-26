c9bbf2549d6cbcbb26f974a33326460296ac41f8
gumodi10@gmail.com <gumodi10@gmail.com>
2022-09-25 20:00:33 -0500

    Add About components

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import './About.css';
+import ME from '../../assets/me-about.jpg';
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {BsFolderPlus} from 'react-icons/bs';
+
 
 const About = () => {
     return (
         <section id="about">
-            About
+            <h5>Get to Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className='about__me-image'>
+                        <img src={ME} alt="About pick" />
+                    </div>
+                </div>
+
+            <div className='about__content'>
+                <div className='about__cards'>
+                    <article className='about__card'>
+                        <FaAward className='about_icon' />
+                        <h5>Experience</h5>
+                        <small>1 Years Working</small>
+                    </article>
+
+                    <article className='about__card'>
+                        <FiUsers className='about_icon' />
+                        <h5>Clients</h5>
+                        <small>10+ Worlwide</small>
+                    </article>
+
+                    <article className='about__card'>
+                        <BsFolderPlus className='about_icon' />
+                        <h5>Projects</h5>
+                        <small>10+ Completed</small>
+                    </article>
+                </div>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make </p>
+
+                <a href="#contact" className="btn btn-primary">Let's Talk</a>
+
+            </div>
+
+            </div>
         </section>
     );
 }

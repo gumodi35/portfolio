92a29d470942bc10b12536e6a5ff812c7bafcdc5
gumodi10@gmail.com <gumodi10@gmail.com>
2022-09-26 17:56:43 -0500

    Add Services component

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,9 +1,116 @@
 import React from 'react';
 import './Services.css';
+import {MdLibraryAddCheck} from 'react-icons/md'
 
 const Services = () => {
     return (
-        <section id="services">Services</section>
+        <section id="services">
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+
+            <div className="container services__container">
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+                     
+                    <ul className="service__list">
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                    </ul>
+                </article>
+
+                {/* Tutorials */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Tutorials</h3>
+                    </div>
+                    
+                    <ul className="service__list">
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                    </ul>
+                </article>
+                
+                {/* Crating Content */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Content Creation</h3>
+                    </div>
+                    
+                    <ul className="service__list">
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                        <li>
+                            <MdLibraryAddCheck className="service__list-icon" />
+                            <p>Lorem, ipsum sit a met consectetur elit</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
     );
 }
 

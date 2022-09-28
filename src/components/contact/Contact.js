2c159b2deef9823be72482cc36bfcd64c166f429
gumodi10@gmail.com <gumodi10@gmail.com>
2022-09-27 20:03:10 -0500

    Add Contact component

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,57 @@
 import React from 'react';
 import './Contact.css';
+import {HiOutlineMail} from 'react-icons/hi';
+import {AiOutlineInstagram} from 'react-icons/ai';
+import {AiOutlineTwitter} from 'react-icons/ai';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_sa3930e', 'template_3vhx5jo', form.current, 'jSzd4Q5UK_EuyEZ_D')
+        
+        e.target.reset()
+      };
+
     return (
-        <section id="contact">Contact</section>
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className='contact__option'>
+                        <HiOutlineMail className='contact__option-icon'/>
+                        <h4>Email</h4>
+                        <h5>gumodi10@gmail.com</h5>
+                        <a href="mailto:gumodi10@gmail.com" target="_blank">Send a Message</a>
+                    </article>
+                    <article className='contact__option'>
+                        <AiOutlineInstagram className='contact__option-icon' />
+                        <h4>Instagram</h4>
+                        <h5>Sr.Gualbert</h5>
+                        <a href="https://m.me/gualbertMontiel.achiever" target="_blank">Send a Message</a>
+                    </article>
+                    <article className='contact__option'>
+                        <AiOutlineTwitter className='contact__option-icon' />
+                        <h4>Twitter</h4>
+                        <h5>Sr.Gualbert</h5>
+                        <a href="mailto:gumodi10@gmail.com" target="_blank">Send a Message</a>
+                    </article>
+                </div>
+        {/* END OF CONTACT OPTIONS */}
+            <form ref={form} onSubmit={sendEmail}>
+                <input type="text" name="name" placeholder='Your full Name' required/>
+                <input type="email" name="email" placeholder='Your Email' required/>
+                <textarea name="message" rows="7" placeholder="Yor Message" required></textarea>
+                <button type="submit" className='btn btn-primary'>Send a Message</button>
+            </form>
+            </div>
+        </section>
     );
 }
 
